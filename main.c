37b7a1e1b36406acc6a6272b990e53ded08d6442
Brian M. Waters <brian@brianmwaters.net>
2015-11-21 05:37:49 -0500

    Revert "Change indentation style"
    This reverts commit d3cd3a46b9ed30652832d0de9195fc76f678f58f.

--- main.c
+++ main.c
@@ -13,121 +13,120 @@
 #include "util.h"
 
 static struct {
-        bool opt_rng_seed;
-        unsigned int arg_rng_seed;
+    bool opt_rng_seed;
+    unsigned int arg_rng_seed;
 } opts;
 
 static void usage(const char *name)
 {
-        fprintf(stderr, "Usage: %s: [-r seed]\n", name);
-        exit(2);
+    fprintf(stderr, "Usage: %s: [-r seed]\n", name);
+    exit(2);
 }
 
 static unsigned int str_to_uint(const char *str, int *err)
 {
-        long long num;
-        char *end = NULL;
+    long long num;
+    char *end = NULL;
 
-        if (isspace(str[0])) {
-                *err = EINVAL;
-                return 0;
-        }
-        // We use the signed version of strtoll to avoid silent negation of
-        // negative inputs (see strtoll(3)). This restricts our input to values
-        // less than LLONG_MAX, which may or may not be less than UINT_MAX. In
-        // practice, this means the user may be unable to set the most
-        // significant bit of the seed on platforms where
-        // sizeof (unsigned int) == sizeof (long long).
-        errno = 0;
-        num = strtoll(str, &end, 10);
-        if (errno != 0) {
-                *err = errno;
-                return 0;
-        } else if (end == str || *end != '\0') {
-                *err = EINVAL;
-                return 0;
-        } else if (num < 0 || num > UINT_MAX) {
-                *err = ERANGE;
-                return 0;
-        }
-        *err = 0;
-        return (unsigned int) num;
+    if (isspace(str[0])) {
+        *err = EINVAL;
+        return 0;
+    }
+    // We use the signed version of strtoll to avoid silent negation of negative
+    // inputs (see strtoll(3)). This restricts our input to values less than
+    // LLONG_MAX, which may or may not be less than UINT_MAX. In practice, this
+    // means the user may be unable to set the most significant bit of the seed
+    // on platforms where sizeof (unsigned int) == sizeof (long long).
+    errno = 0;
+    num = strtoll(str, &end, 10);
+    if (errno != 0) {
+        *err = errno;
+        return 0;
+    } else if (end == str || *end != '\0') {
+        *err = EINVAL;
+        return 0;
+    } else if (num < 0 || num > UINT_MAX) {
+        *err = ERANGE;
+        return 0;
+    }
+    *err = 0;
+    return (unsigned int) num;
 }
 
 static void get_opts(int argc, char **argv)
 {
-        int opt;
-        int err;
+    int opt;
+    int err;
 
-        opts.opt_rng_seed = false;
-        opterr = 0; // suppress getopt error messages. see getopt(3)
-        while ((opt = getopt(argc, argv, "r:")) != -1) {
-                switch ((char) opt) {
-                case 'r':
-                        opts.opt_rng_seed = true;
-                        opts.arg_rng_seed = str_to_uint(optarg, &err);
-                        if (err != 0) {
-                                usage(argv[0]);
-                        }
-                        break;
-                default:
-                        usage(argv[0]);
-                }
-        }
-        if (optind != argc) {
+    opts.opt_rng_seed = false;
+    opterr = 0; // suppress getopt error messages. see getopt(3)
+    while ((opt = getopt(argc, argv, "r:")) != -1) {
+        switch ((char) opt) {
+        case 'r':
+            opts.opt_rng_seed = true;
+            opts.arg_rng_seed = str_to_uint(optarg, &err);
+            if (err != 0) {
                 usage(argv[0]);
+            }
+            break;
+        default:
+            usage(argv[0]);
         }
+    }
+    if (optind != argc) {
+        usage(argv[0]);
+    }
 }
 
 static void seed_rng(void)
 {
-        unsigned int seed;
-        time_t cur;
+    unsigned int seed;
+    time_t cur;
 
-        if (opts.opt_rng_seed) {
-                seed = opts.arg_rng_seed;
-        } else {
-                cur = time(NULL);
-                if (cur == -1) {
-                        fail("Could not get current time", 0);
-                }
-                seed = (unsigned int) cur;
+    if (opts.opt_rng_seed) {
+        seed = opts.arg_rng_seed;
+    } else {
+        cur = time(NULL);
+        if (cur == -1) {
+            fail("Could not get current time", 0);
         }
-        srand(seed);
+        seed = (unsigned int) cur;
+    }
+    srand(seed);
 }
 
 static bool detect_and_display(bool (*test)(void), const char *name)
 {
-        bool result;
-        int err;
+    bool result;
+    int err;
 
-        result = test();
-        err = printf("%s: %s\n", result ? "PASS" : "FAIL", name);
-        if (err < 0) {
-                fail("Error printing to stdout", errno);
-        }
-        return result;
+    result = test();
+    err = printf("%s: %s\n", result ? "PASS" : "FAIL", name);
+    if (err < 0) {
+        fail("Error printing to stdout", errno);
+    }
+    return result;
 }
 
 static bool detect_mitigations(void)
 {
-        bool result = true;
+    bool result = true;
 
-        result &= detect_and_display(detect_stack_exec_prevent,
-                "Stack segment execution prevention");
-        result &= detect_and_display(detect_heap_exec_prevent,
-                "Heap segment execution prevention");
-        result &= detect_and_display(detect_data_exec_prevent,
-                "Data segment execution prevention");
-        return result;
+    result &= detect_and_display(detect_stack_exec_prevent,
+        "Stack segment execution prevention");
+    result &= detect_and_display(detect_heap_exec_prevent,
+        "Heap segment execution prevention");
+    result &= detect_and_display(detect_data_exec_prevent,
+        "Data segment execution prevention");
+    return result;
 }
 
 int main(int argc, char **argv)
 {
-        bool result;
+    bool result;
 
-        get_opts(argc, argv);
-        seed_rng();
-        result = detect_mitigations();
-        return result ? EXIT_SUCCESS : EXIT_FAILURE;
+    get_opts(argc, argv);
+    seed_rng();
+    result = detect_mitigations();
+    return result ? EXIT_SUCCESS : EXIT_FAILURE;
 }
